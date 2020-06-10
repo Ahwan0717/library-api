82584e9aeaccdbe972a69e1fa55d0eb9e1570b9f
Ariana Hwang <ajhwang0717@gmail.com>
2020-06-10 16:27:22 -0400

    feature: set up server and database

---
+++ server/db/models/users.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  isLibrarian: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
+})
+
+module.exports = User
---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const db = require('./db')
+
+const app = express();
+module.exports = app
+
+const createApp = () => {
+
+  //logging middleware
+  app.use(morgan('dev'));
+
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  app.use('/api', require('./apiRoutes'))
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  app.listen(3000, () => {
+    console.log('server has started on port 3000');
+  });
+}
+
+const syncDb = () => db.sync()
+
+async function bootApp() {
+  await syncDb()
+  await createApp()
+  await startListening()
+}
+
+if (require.main === module) {
+  bootApp()
+} else {
+  createApp()
+}
